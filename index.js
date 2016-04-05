6fff435749b07ba632f2884bf744dd9f1a673228
VERMICULITE <seanowenhayes@gmail.com>
2016-04-05 19:20:01 +0000

    much the same

--- index.js
+++ index.js
@@ -1,19 +1,8 @@
 "use strict";
 
-let React = require('react');
-let PropTypes = React.PropTypes;
-
-class ReactDDP extends React.Component {
-
-    constructor(props) {
-        super(props);
+class MiniMeteor() {
+    
+    constructor(endPoint) {
+        this.ws = new WebSocket(endPoint);
     }
 }
-
-ReactDDP.propTypes = {
-    url: PropTypes.string.is.Required   
-};
-
-
-
-module.exports = ReactDDP;
